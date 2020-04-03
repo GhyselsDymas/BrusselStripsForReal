f851e5464004d8ab78e9598174309507491791b2
mobapp11 <mobapp11@student.ehb.be>
2020-04-03 15:59:19 +0200

    preferences

---
+++ app/src/main/java/com/example/brusselstripsforreal/fragments/SettingsFrangment.java
@@ -0,0 +1,24 @@
+package com.example.brusselstripsforreal.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceFragmentCompat;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.brusselstripsforreal.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SettingsFrangment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
