ec8ddf6ac891e4157c77d2c58c39189b12f106a9
mobapp11 <mobapp11@student.ehb.be>
2020-04-08 13:00:14 +0200

    made artdatabase

--- app/src/main/java/com/example/brusselstripsforreal/model/ArrayAdapter.java
+++
@@ -1,9 +0,0 @@
-package com.example.brusselstripsforreal.model;
-
-public class ArrayAdapter {
-
-    //TODO, replace this with database and DAO
-
-    public static void add(ComicArt currentArt) {
-    }
-}
