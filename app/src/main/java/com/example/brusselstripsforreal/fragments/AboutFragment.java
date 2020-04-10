a7a54feeca7ac5ff51ca6b6ae5c3bdf65352edee
mobapp11 <mobapp11@student.ehb.be>
2020-04-10 14:45:32 +0200

    made api page fixed about page

--- app/src/main/java/com/example/brusselstripsforreal/fragments/AboutFragment.java
+++ app/src/main/java/com/example/brusselstripsforreal/fragments/AboutFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.brusselstripsforreal.R;
 
@@ -14,6 +15,9 @@ import com.example.brusselstripsforreal.R;
  * A simple {@link Fragment} subclass.
  */
 public class AboutFragment extends Fragment {
+
+    private View rootview;
+    private TextView textView;
 
     public AboutFragment() {
         // Required empty public constructor
@@ -24,6 +28,9 @@ public class AboutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        rootview = inflater.inflate(R.layout.fragment_about, container, false);
+        textView = rootview.findViewById(R.id.textView);
+        textView.setText("Brussels loves comic strips so much it has invited its heroes to take possession of its walls and gables. Discover all the details and mysteries here! ….  Go ahead and hunt them down, walk the Brussels streets and raise your eyes! A joyful stroll for enthusiasts and the inquisitive from 7 to 77 years of age.  The brochure « Brussels, the comic strip capital » will help you to find all the murals. On sale at the visit.brussels information desks (2.5€).");
+        return rootview;
     }
 }
